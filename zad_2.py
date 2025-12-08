f753e01696556c72da0433be750816777165d162
ajkenek <ajken2115@gmail.com>
2025-12-08 12:09:50 +0100

    poprawione pycodestyle

--- zad_2.py
+++ zad_2.py
@@ -1,4 +1,5 @@
 class Student:
+
     def __init__(self, name, marks):
         self.name = name
         self.marks = marks
@@ -6,7 +7,9 @@ class Student:
     def __str__(self):
         return f"Student {self.name}"
 
+
 class Library:
+
     def __init__(self, city, street, zip_code, open_hours, phone):
         self.city = city
         self.street = street
@@ -17,7 +20,9 @@ class Library:
     def __str__(self):
         return f'Bibliotetka w {self.city}, {self.street}, {self.zip_code}, godziny otwarcia: {self.open_hours}, telefon: {self.phone}'
 
+
 class Employee:
+
     def __init__(self, first_name, last_name, hire_date, birth_date ,city, street, zip_code, phone):
         self.first_name = first_name
         self.last_name = last_name
@@ -29,9 +34,12 @@ class Employee:
         self.phone = phone
 
     def __str__(self):
+
         return f'Pracownik {self.first_name} {self.last_name}'
 
+
 class Book:
+
     def __init__(self, library, publication_date, author_name, author_surname, number_pages):
         self.library = library
         self.publication_date = publication_date
@@ -42,7 +50,9 @@ class Book:
     def __str__(self):
         return f'Książka, autor: {self.author_name} {self.author_surname}, pages: {self.number_pages}'
 
+
 class Order:
+
     def __init__(self, employee, student, books, order_date):
         self.employee = employee
         self.student = student
